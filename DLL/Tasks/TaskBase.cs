1585e2ad462aca371a523285e73e59ac8500ef39
Latency McLaughlin <latency@bio-hazard.us>
2020-09-13 12:39:28 -0700

    Update for build and release.

--- DLL/Tasks/TaskBase.cs
+++ DLL/Tasks/TaskBase.cs
@@ -206,7 +206,6 @@ namespace AsyncTask.Tasks
             catch (ThreadAbortException)
             {
                 Thread.ResetAbort();
-                throw new TaskCanceledException();
             }
         }, Token);
     }
